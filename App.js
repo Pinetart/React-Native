704b9c7c07d528030a8cf73a80cbb89c6178e031
Zane Birkett <axelbirkett@gmail.com>
2022-08-08 14:53:00 -0400

    Starting the Reviews App

--- App.js
+++ App.js
@@ -1,82 +1,8 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Alert,
-  View,
-  FlatList,
-  TouchableWithoutFeedback,
-  Keyboard,
-} from "react-native";
-import AddTodo from "./components/addTodo";
-import Header from "./components/header";
-import Sandbox from "./components/sandbox";
-import TodoItem from "./components/todoItem";
+import React from "react";
+import Home from "./screens/home";
+import About from "./screens/about";
+import ReviewDetails from "./screens/reviewDetails";
 
 export default function App() {
-  const [todos, setTodos] = useState([
-    { text: "buy coffee", key: "1" },
-    { text: "create an app", key: "2" },
-    { text: "play on the switch", key: "3" },
-    { text: "testing the app", key: "4" },
-  ]);
-  const submitHandler = (text) => {
-    if (text.length > 3) {
-      setTodos((prevTodos) => {
-        return [{ text: text, key: Math.random().toString() }, ...prevTodos];
-      });
-    } else {
-      Alert.alert("OOPS!", "Todos must be over 3 characters long", [
-        {
-          text: "Ok",
-        },
-      ]);
-    }
-  };
-
-  const pressHandler = (key) => {
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.key != key));
-  };
-
-  return (
-    // <Sandbox />
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-      }}
-    >
-      <View style={styles.container}>
-        <Header title="My Todos" />
-        <View style={styles.content}>
-          <AddTodo submitHandler={submitHandler} />
-          <View style={styles.list}>
-            <FlatList
-              style={styles["flist"]}
-              data={todos}
-              renderItem={({ item }) => (
-                <TodoItem item={item} pressHandler={pressHandler} />
-              )}
-            />
-          </View>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  content: {
-    flex: 1,
-    padding: 40,
-  },
-  list: {
-    flex: 1,
-    marginTop: 20,
-  },
-  flist: {
-    paddingHorizontal: 20,
-  },
-});
